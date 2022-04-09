ce91477deaee1a964564a7b6bb4dac571cf0d6e3
Adaxator <adax324@gmail.com>
2022-04-09 20:25:36 +0200

    0.1

--- src/main/java/com/drivingschool/configuration/SpringConfiguration.java
+++ src/main/java/com/drivingschool/configuration/SpringConfiguration.java
@@ -5,26 +5,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SpringConfiguration {
+@EnableWebMvc
+public class SpringConfiguration implements WebMvcConfigurer{
     @Autowired
     private AppUrl appUrl;
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET","PUT","POST","DELETE")
-                        .allowedHeaders("*")
-                        .allowedOrigins(appUrl.getUrl());
-            }
-        };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
+
+    //    @Bean
+//    public WebMvcConfigurer webMvcConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("GET","PUT","POST","DELETE")
+//                        .allowedHeaders("*")
+//                        .allowedOrigins(appUrl.getUrl());
+//            }
+//        };
+//    }
 }
